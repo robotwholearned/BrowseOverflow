ab0e00017f7a18cd093c0da0bc8a613ffb8344be
robotwholearned <robotwholearned88@gmail.com>
2014-06-20 23:14:02 -0600

    person test set up

--- BrowseOverflowTests/PearsonTests.swift
+++ BrowseOverflowTests/PearsonTests.swift
@@ -9,27 +9,11 @@
 import XCTest
 
 class PearsonTests: XCTestCase {
-
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  
+  
+  override func setUp() {
+    super.setUp()
+    var person = Person(name:"Cassandra Sandquist ", avatarLocation: "https://www.gravatar.com/avatar/4859f078c642a60853c3d2752cbfee4f?s=128&d=identicon&r=PG")
+  }
+  
 }

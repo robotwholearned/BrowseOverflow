d52fa82d80fa63a0a53fba665f6db698300a3d73
robotwholearned <robotwholearned88@gmail.com>
2014-06-20 22:48:46 -0600

    add question class

---
+++ BrowseOverflow/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  BrowseOverflow
+//
+//  Created by Cassandra Sandquist on 2014-06-20.
+//  Copyright (c) 2014 Cassandra Sandquist. All rights reserved.
+//
+
+import Foundation
+
+class Question{
+  var date = NSDate().dateByAddingTimeInterval(0)
+}

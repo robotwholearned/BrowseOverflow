a6bd462c6160699b69fcac0e86074fb841119c04
robotwholearned <robotwholearned88@gmail.com>
2014-06-20 23:03:20 -0600

    use setup function for common needs

--- BrowseOverflowTests/QuestionTests.swift
+++ BrowseOverflowTests/QuestionTests.swift
@@ -9,12 +9,20 @@
 import XCTest
 
 class QuestionTests: XCTestCase {
-
-  func testQuestionHasDate(){
-    var question = Question()
-    var testDate = NSDate(timeIntervalSince1970: 0)
+  
+  var question = Question()
+  var testDate = NSDate(timeIntervalSince1970: 0)
+  
+  override func setUp() {
+    super.setUp()
     question.date = testDate
-    XCTAssertEqualObjects(question.date, testDate, "Questions needs to provide a date")
+    question.title = "Do iPhones also dream of electric sheep?"
+    question.score = 42
   }
 
+  func testQuestionHasDate(){
+    XCTAssertEqualObjects(question.date, testDate, "Questions needs to provide a date")
+  }
+  
+  
 }

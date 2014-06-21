d128f692bbe0d284ca289e8c3a1ffe475a371a0b
robotwholearned <robotwholearned88@gmail.com>
2014-06-20 23:17:09 -0600

    add person class with name and avatarLocation

---
+++ BrowseOverflow/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  BrowseOverflow
+//
+//  Created by Cassandra Sandquist on 2014-06-20.
+//  Copyright (c) 2014 Cassandra Sandquist. All rights reserved.
+//
+
+import Foundation
+
+class Person{
+  var name : String
+  var avatarLocation : String
+  
+  init(name: String, avatarLocation: String){
+    self.name = name
+    self.avatarLocation = avatarLocation
+  }
+  
+}

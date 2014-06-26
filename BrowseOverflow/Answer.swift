2d3c797be7db99744439b1e913b0740baebd07f1
robotwholearned <robotwholearned88@gmail.com>
2014-06-26 11:35:42 -0600

    Add answer class

---
+++ BrowseOverflow/Answer.swift
@@ -0,0 +1,13 @@
+//
+//  Answer.swift
+//  BrowseOverflow
+//
+//  Created by Cassandra Sandquist on 2014-06-26.
+//  Copyright (c) 2014 Cassandra Sandquist. All rights reserved.
+//
+
+import Foundation
+
+class Answer{
+  
+}

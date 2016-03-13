f7c498979c6610c4db636cc2fab292dc2790cea8
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-13 10:51:47 -0400

    adds Topic class to application target

---
+++ BrowseOverflow/Topic.swift
@@ -0,0 +1,11 @@
+//
+//  Topic.swift
+//  BrowseOverflow
+//
+//  Created by Sandquist, Cassandra - Cassandra on 3/13/16.
+//  Copyright © 2016 robotwholearned. All rights reserved.
+//
+
+class Topic {
+
+}

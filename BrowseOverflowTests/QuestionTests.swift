6cdce3688f6eccdf834b2c3319777eb2a0152ae3
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-13 13:06:23 -0400

    adds tests for questions

---
+++ BrowseOverflowTests/QuestionTests.swift
@@ -0,0 +1,23 @@
+//
+//  QuestionTests.swift
+//  BrowseOverflow
+//
+//  Created by Sandquist, Cassandra - Cassandra on 3/13/16.
+//  Copyright © 2016 robotwholearned. All rights reserved.
+//
+
+import XCTest
+
+class QuestionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}

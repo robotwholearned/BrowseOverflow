bc56ac771c665e63ab8347d79f0690413472661b
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-13 13:22:29 -0400

    adds persontests

---
+++ BrowseOverflowTests/PersonTests.swift
@@ -0,0 +1,23 @@
+//
+//  PersonTests.swift
+//  BrowseOverflow
+//
+//  Created by Sandquist, Cassandra - Cassandra on 3/13/16.
+//  Copyright © 2016 robotwholearned. All rights reserved.
+//
+
+import XCTest
+@testable import BrowseOverflow
+
+class PersonTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}

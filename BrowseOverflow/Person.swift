8d35c4a465ac455b6ccb5ae89d2dcb6820ed451f
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2016-03-13 13:35:39 -0400

    adds person class

---
+++ BrowseOverflow/Person.swift
@@ -0,0 +1,9 @@
+//
+//  Person.swift
+//  BrowseOverflow
+//
+//  Created by Sandquist, Cassandra - Cassandra on 3/13/16.
+//  Copyright © 2016 robotwholearned. All rights reserved.
+//
+
+import Foundation
